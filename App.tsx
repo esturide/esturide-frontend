de98ddfbe0455356501b965fada1b3498c991e5e
Gesem-Martinez <109618325+Gesem-Martinez@users.noreply.github.com>
2024-01-20 14:58:30 -0600

    Add background image and logo to login screen.

--- App.tsx
+++ App.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import { View, SafeAreaView, Text } from 'react-native';
+import Login from './src/screens/Login';
 
 const App: Element = () => {
-  return (
-    <SafeAreaView>
-      <Text>Prueba</Text>
-    </SafeAreaView>
-  );
+  return <Login />;
 };
 
 export default App;
--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,3 +1,47 @@
 import React from 'react';
-import { SafeAreaView, View, Text } from 'react-native/types'; 
+import {
+  SafeAreaView,
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  ImageBackground
+} from 'react-native';
 
+const Login: Element = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <ImageBackground
+        source={require('../../assets/fondo-peces.jpg')}
+        style={styles.bgImageLayout}>
+        <View style={styles.logoContainer}>
+          <Image
+            source={require('../../assets/logo.png')}
+            style={styles.logo}
+          />
+        </View>
+        <View style={styles.content}></View>
+      </ImageBackground>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  bgImageLayout: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  logoContainer: {
+    maxWidth: '80%',
+    alignItems: 'center'
+  },
+  logo: {
+    width: 400,
+    height: 400
+  }
+});
+
+export default Login;
