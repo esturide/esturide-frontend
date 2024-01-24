64f730906d15fb6462916abf1679492ede5235e0
Gesem-Martinez <109618325+Gesem-Martinez@users.noreply.github.com>
2024-01-24 10:54:45 -0600

    Add react-navigation library

--- App.tsx
+++ App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { View, SafeAreaView, Text } from 'react-native';
 import Login from './src/screens/Login.tsx';
+import UserProfile from './src/screens/UserProfile.tsx';
+import { NavigationContainer } from '@react-navigation/native';
 
 const App: Element = () => {
-  return <Login />;
+  return (
+    <NavigationContainer>
+      <UserProfile />
+    </NavigationContainer>
+  );
 };
 
 export default App;
