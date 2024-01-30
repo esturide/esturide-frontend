5dea16f6a6ba378d811eafd21720211c410945e7
SrpequeFlukey25 <santiagoc4ssielhdez@gmail.com>
2024-01-30 03:28:27 -0600

    Pantallas Registro Bienvenida
    En esta actualizacion agregué las pantallas de registro de usuario (no de vehiculo) y la pantalla de bienvenida

---
+++ src/screens/RegistroNuevoUsuario.tsx
@@ -0,0 +1,178 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  ImageBackground,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
+import InputBox from '../../src/components/InputBox';
+import Logo from '../../src/components/Logo';
+
+const RegistroNuevoUsuario = () => {
+  return (
+    <ImageBackground
+      source={require('../../assets/cut-Bg.jpg')}
+      blurRadius={5}
+      style={styles.backgroundImage}>
+      <View style={styles.container}>
+        <Logo />
+        <View style={styles.bandaDatos}>
+          <Text style={styles.titulo}>Datos de Usuario</Text>
+        </View>
+        <View style={styles.inputZone}>
+          <View style={styles.row}>
+            {/* Nombre de Usuario */}
+            <InputBox
+              label="Nombre de Usuario"
+              labelStyles={styles.labelStyles}
+              inputWidth={150}
+              secureTextEntry={false}
+              marginVertical={10}
+              marginHorizontal={5}
+            />
+            {/* Correo Electrónico */}
+            <InputBox
+              label="Correo Electrónico"
+              labelStyles={styles.labelStyles}
+              inputWidth={150}
+              secureTextEntry={false}
+              marginVertical={10}
+              marginHorizontal={5}
+            />
+          </View>
+          <View style={styles.row}>
+            {/* Contraseña */}
+            <InputBox
+              label="Contraseña"
+              labelStyles={styles.labelStyles}
+              inputWidth={150}
+              secureTextEntry={true}
+              marginVertical={10}
+              marginHorizontal={5}
+            />
+            {/* Confirmar Contraseña */}
+            <InputBox
+              label="Confirmar Contraseña"
+              labelStyles={styles.labelStyles}
+              inputWidth={150}
+              secureTextEntry={true}
+              marginVertical={10}
+              marginHorizontal={5}
+            />
+          </View>
+        </View>
+        <ButtonSiguiente />
+        <ButtonVolver />
+      </View>
+    </ImageBackground>
+  );
+};
+
+const ButtonSiguiente = () => {
+  return (
+    <View>
+      <Text>
+        <TouchableOpacity
+          style={styles.botonSiguiente}
+          onPress={() => alert('Estas seguro que quieres avanzar?')}>
+          <Text style={styles.textoboton}>Siguiente</Text>
+        </TouchableOpacity>
+      </Text>
+    </View>
+  );
+};
+const ButtonVolver = () => {
+  return (
+    <View>
+      <TouchableOpacity
+        style={styles.botonVolver}
+        onPress={() => alert('Estas seguro que quieres avanzar?')}>
+        <Text style={styles.textoboton}>Volver</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    resizeMode: 'cover',
+    height: 'auto',
+    width: 'auto',
+  },
+  labelStyles: {
+    color: 'black',
+    letterSpacing: 5,
+  },
+  botonSiguiente: {
+    backgroundColor: '#0096c7',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 20,
+    borderColor: '#05769c',
+    borderWidth: 2,
+    margin: 10,
+  },
+  botonVolver: {
+    backgroundColor: '#023e8a',
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    borderRadius: 20,
+    borderColor: '#04306a',
+    borderWidth: 2,
+    margin: 10,
+  },
+  textoboton: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+    letterSpacing: 8,
+    textShadowColor: 'black',
+    textShadowOffset: {width: 1, height: 1},
+    textShadowRadius: 2,
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  bandaDatos: {
+    position: 'absolute',
+    backgroundColor: 'rgba(0, 0, 0, 0.3)', // Fondo negro semi-opaco
+    top: 380,
+    bottom: 420,
+    left: -50,
+    right: -50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  titulo: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: 'white',
+    letterSpacing: 8,
+    textShadowColor: 'black',
+    textShadowOffset: {width: 1, height: 1},
+    textShadowRadius: 2,
+  },
+  inputZone: {
+    backgroundColor: 'white',
+    borderRadius: 20,
+    borderColor: 'black',
+    borderWidth: 3,
+    marginTop: 30,
+    marginBottom: 20,
+  },
+});
+
+export default RegistroNuevoUsuario;
