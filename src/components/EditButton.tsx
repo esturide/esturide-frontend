42058802097ef2f946f2d37168ca29d94b01264a
Gesem-Martinez <109618325+Gesem-Martinez@users.noreply.github.com>
2024-02-26 23:13:19 -0600

    Add input boxes and date picker components

--- src/components/EditButton.tsx
+++ src/components/EditButton.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { View, TouchableOpacity, StyleSheet } from 'react-native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-const EditButton = () => {
+const EditButton = ({ onPress }) => {
   return (
     <View style={styles.editBtn}>
-      <TouchableOpacity>
+      <TouchableOpacity onPress={onPress}>
         <MaterialCommunityIcons name={'pencil'} color={'black'} size={30} />
       </TouchableOpacity>
     </View>
