d5614c6f1d7351924811f1a2b78f16fa180c92e0
Gesem-Martinez <109618325+Gesem-Martinez@users.noreply.github.com>
2024-02-27 01:07:39 -0600

    Add input validation

--- src/screens/EditProfile.tsx
+++ src/screens/EditProfile.tsx
@@ -11,10 +11,26 @@ import InputBox from '../components/InputBox';
 import DateTimePicker from 'react-native-ui-datepicker';
 import 'dayjs/locale/es-mx';
 import dayjs from 'dayjs';
+import * as yup from 'yup';
+import { Controller, useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+
+const formSchema = yup.object({
+  nombre: yup.string().required('Campo Obligatorio'),
+  apellidoPaterno: yup.string().required('Campo Obligatorio'),
+  apellidoMaterno: yup.string().required('Campo Obligatorio'),
+  fechaNacimiento: yup.date().required('Campo Obligatorio')
+});
 
 const EditProfile = ({ modalState, setModalState }) => {
-
   const [date, setDate] = useState(dayjs());
+  const {
+    control,
+    handleSubmit,
+    formState: { errors }
+  } = useForm({
+    resolver: yupResolver(formSchema)
+  });
 
   return (
     <Modal
@@ -43,52 +59,103 @@ const EditProfile = ({ modalState, setModalState }) => {
               width: 350
             }}
           >
-            <InputBox
-              label={'Nombre:'}
-              labelStyles={styles.editLabels}
-              secureTextEntry={false}
-              inputWidth={350}
-              marginVertical={20}
+            <Controller
+              control={control}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <InputBox
+                  onChange={onChange}
+                  onBlur={onBlur}
+                  value={value}
+                  label={'Nombre:'}
+                  labelStyles={styles.editLabels}
+                  secureTextEntry={false}
+                  inputWidth={350}
+                  marginVertical={20}
+                  errors={errors.nombre}
+                />
+              )}
+              name="nombre"
+              defaultValue=""
             />
-            <InputBox
-              label={'Apellido Paterno:'}
-              labelStyles={styles.editLabels}
-              secureTextEntry={false}
-              inputWidth={160}
-              marginVertical={20}
+            <Controller
+              control={control}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <InputBox
+                  onChange={onChange}
+                  onBlur={onBlur}
+                  value={value}
+                  label={'Apellido Paterno:'}
+                  labelStyles={styles.editLabels}
+                  secureTextEntry={false}
+                  inputWidth={160}
+                  marginVertical={20}
+                  errors={errors.apellidoPaterno}
+                />
+              )}
+              name="apellidoPaterno"
+              defaultValue=""
             />
-            <InputBox
-              label={'Apellido Materno:'}
-              labelStyles={styles.editLabels}
-              secureTextEntry={false}
-              inputWidth={160}
-              marginVertical={20}
+            <Controller
+              control={control}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <InputBox
+                  onChange={onChange}
+                  onBlur={onBlur}
+                  value={value}
+                  label={'Apellido Materno:'}
+                  labelStyles={styles.editLabels}
+                  secureTextEntry={false}
+                  inputWidth={160}
+                  marginVertical={20}
+                  errors={errors.apellidoMaterno}
+                />
+              )}
+              name="apellidoMaterno"
+              defaultValue=""
             />
           </View>
           <View style={{ marginVertical: 20 }}>
             <Text style={styles.editLabels}>Fecha de Nacimiento:</Text>
-            <View style={styles.datePickerContainer}>
-              <DateTimePicker
-                mode={'single'}
-                date={date}
-                onChange={(params) => setDate(params.date)}
-                calendarTextStyle={{ color: 'black' }}
-                headerTextStyle={{
-                  color: 'black',
-                  textTransform: 'capitalize'
-                }}
-                headerTextContainerStyle={{ color: 'black' }}
-                weekDaysTextStyle={{
-                  color: 'black',
-                  textTransform: 'capitalize'
-                }}
-                locale={'es-mx'}
-              />
-            </View>
+            {errors.fechaNacimiento && (
+              <Text style={{ color: 'red', fontSize: 12, marginTop: 10 }}>
+                {errors.fechaNacimiento.message}
+              </Text>
+            )}
+            <Controller
+              control={control}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <View style={styles.datePickerContainer}>
+                  <DateTimePicker
+                    mode={'single'}
+                    date={value}
+                    onChange={(params) => onChange(params.date)}
+                    calendarTextStyle={{ color: 'black' }}
+                    headerTextStyle={{
+                      color: 'black',
+                      textTransform: 'capitalize'
+                    }}
+                    headerTextContainerStyle={{ color: 'black' }}
+                    weekDaysTextStyle={{
+                      color: 'black',
+                      textTransform: 'capitalize'
+                    }}
+                    locale={'es-mx'}
+                  />
+                </View>
+              )}
+              name="fechaNacimiento"
+              defaultValue={dayjs().toDate()}
+            />
           </View>
 
           <View style={styles.editProfileButtons}>
-            <TouchableOpacity style={styles.editSubmitBtn}>
+            <TouchableOpacity
+              style={styles.editSubmitBtn}
+              onPress={handleSubmit((data) => {
+                console.log('AAAAAAAAAAAAAAAAAAAAAAAAAAAAA');
+                setModalState(false);
+              })}
+            >
               <Text style={styles.editSubmitText}>Guardar Cambios</Text>
             </TouchableOpacity>
           </View>
