629b3d507537a95bec35243ea3471e8686734d26
Gesem-Martinez <109618325+Gesem-Martinez@users.noreply.github.com>
2024-03-07 10:34:32 -0600

    Add localhost variable to fetch data

--- src/hooks/userGetUserData.tsx
+++ src/hooks/userGetUserData.tsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from 'react';
 
 export const useGetUserData = () => {
+  const localhost = '10.9.185.187';
   const [loading, setLoading] = useState(true);
   const [userData, setUserData] = useState([]);
+  const [err, setErr] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const result = await fetch('http://172.19.243.59:8000/users/1', {
+        const result = await fetch(`http://${localhost}:8000/users/1`, {
           method: 'GET',
           headers: {
             Authorization: `Bearer ${'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxMDEsImV4cCI6MTcxMTY2MjY3OH0.yJEfuBfNQVsgvfNaXwb0CfzgH_UUOUHowO5iLnk_sNo'}`
@@ -15,8 +17,9 @@ export const useGetUserData = () => {
         });
         const data = await result.json();
         setUserData(data);
-      } catch (err) {
-        console.log('Error fetching data', err);
+      } catch (error) {
+        console.log('Error fetching data', error);
+        setErr(error);
       } finally {
         setLoading(false);
       }
